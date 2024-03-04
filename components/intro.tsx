c8e271699b6002acb9bec9ac7bd4a5386afb15c1
bagasyudhistira <bagasyudhistira64@gmail.com>
2024-03-04 23:15:37 +0700

    remove CV

--- components/intro.tsx
+++ components/intro.tsx
@@ -88,14 +88,14 @@ export default function Intro() {
           <BsArrowRight className="opacity-75 group-hover: translate-x-1 transition" />
         </Link>
 
-        <a
+        {/* <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
           href="/BagasYudhistiraFauzi_CV.pdf"
           download={true}
         >
           Download CV (ATS-friendly){" "}
           <HiDownload className=" opacity-75 group-hover:translate-y-1 transition" />
-        </a>
+        </a> */}
 
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 rounded-full focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
